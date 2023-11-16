f5b3fe9a1ab4690ca7aac01467f3c6083e59c819
Omkar Patil <omkar.spatil@outlook.com>
2023-11-17 00:30:07 +0530

    Fixes Minor Error

--- chess.py
+++ chess.py
@@ -1,6 +1,9 @@
 # Building Two Player Chess Game using Pygame
 
+# Importing Modules
 import pygame
+import sys 
+
 pygame.init()
 
 # Variables
@@ -18,12 +21,19 @@ pygame.display.set_caption("Satranj Ka Khel")
 icon = pygame.image.load("ProjectImages/logo.png")
 pygame.display.set_icon(icon)
 
+#Optional FPS ajduste(no such need of fps manager here since game is not animated type)
+speed = pygame.time.Clock()
 
 # Game Starts From Here -->>
 while running:
     for Events in pygame.event.get():
-        if Events.type == pygame.QUIT:
-            pygame.quit() 
+        if Events.type == pygame.QUIT: # Functionality to close button
+            pygame.quit()    # Just deinitializes library(can get error in further pygame instructions).
+            sys.exit()
 
+    speed.tick(30)      # Game will be run at 30fps.
     pygame.display.flip()
 
+
+pygame.quit()
+sys.exit()
